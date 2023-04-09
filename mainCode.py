801ff62b3e1c848f2c96dd7baf77592c987b9b0e
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2023-04-09 09:46:34 -0500

    Update mainCode.py

--- mainCode.py
+++ mainCode.py
@@ -6,5 +6,16 @@ from tkinter import *
 from tkinter import messagebox
 from random import *
 
+
+def main():
+    program = RockPaperScissors()
+    program.root.mainloop()
+
+
+class RockPaperScissors:
+    def __init__(self):
+        self.root = Tk()
+
+
 if __name__ == "main":
-    pass
+    main()
