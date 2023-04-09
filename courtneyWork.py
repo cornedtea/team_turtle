6c15c183fe867227f288fe5972762ccefbd9195c
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2023-04-09 09:46:40 -0500

    Update courtneyWork.py

--- courtneyWork.py
+++ courtneyWork.py
@@ -2,72 +2,49 @@
 File with Courtney's code
 """
 
-from mainCode import *
+from tkinter import *
+from tkinter import messagebox
+from random import *
 
 
 def main():
     program = RockPaperScissors()
-    program.window.mainloop()
+    program.root.mainloop()
 
 
 class RockPaperScissors:
-    initialize ‘number
-    of
-    objects
-    per
-    team’ value
-    set
-    up
-    background
-    color
-    variable
+    # initialize ‘number of objects per team’ value
+    # set up background color variable
 
+    def __init__(self):
+        self.root = Tk()
+        # name window
+        # create canvas and interface frames
+        # set up ‘scissors’ image as label
+        # set up ‘rock’ image as label
+        # set up ‘paper’ image as label
+        # set up “Set up”, “Start”, and “Stop” buttons
+        # initialize “is_running” Boolean as False
+        # set up callback functions
 
-def __init__(self):
-    create
-    window
-    name
-    window
-    create
-    canvas and interface
-    frames
-    set
-    up ‘scissors’ image as label
-    set
-    up ‘rock’ image as label
-    set
-    up ‘paper’ image as label
-    set
-    up “Set
-    up”, “Start”, and “Stop” buttons
-    initialize “is_running” Boolean as False
-    set
-    up
-    callback
-    functions
+    # function to create frames
 
+    # button functions
 
-function
-to
-create
-frames
+    # callback functions
 
-# button functions
+    # animation function
 
-# callback functions
+    # collision function
 
-# animation function
+    def endResult(self):
+        # save name of winning team as ‘winner’
+        #if guess is the same as ‘winner’:
+        #     messagebox with congratulatory message
+        # else:
+        #     message box with better luck next time message
+        pass
 
-# collision function
-
-
-def endResult():
-    # save name of winning team as ‘winner’
-    #if guess is the same as ‘winner’:
-    #     messagebox with congratulatory message
-    # else:
-    #     message box with better luck next time message
-    pass
 
 if __name__ == "main":
-    pass
+    main()
