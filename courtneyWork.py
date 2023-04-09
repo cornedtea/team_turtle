ceb67d4b147bb4d7ac94a04ad025126605b7fd7b
Courtney Brown <116745836+cornedtea@users.noreply.github.com>
2023-04-09 09:27:20 -0500

    Update courtneyWork.py

--- courtneyWork.py
+++ courtneyWork.py
@@ -4,5 +4,70 @@ File with Courtney's code
 
 from mainCode import *
 
+
+def main():
+    program = RockPaperScissors()
+    program.window.mainloop()
+
+
+class RockPaperScissors:
+    initialize ‘number
+    of
+    objects
+    per
+    team’ value
+    set
+    up
+    background
+    color
+    variable
+
+
+def __init__(self):
+    create
+    window
+    name
+    window
+    create
+    canvas and interface
+    frames
+    set
+    up ‘scissors’ image as label
+    set
+    up ‘rock’ image as label
+    set
+    up ‘paper’ image as label
+    set
+    up “Set
+    up”, “Start”, and “Stop” buttons
+    initialize “is_running” Boolean as False
+    set
+    up
+    callback
+    functions
+
+
+function
+to
+create
+frames
+
+# button functions
+
+# callback functions
+
+# animation function
+
+# collision function
+
+
+def endResult():
+    # save name of winning team as ‘winner’
+    #if guess is the same as ‘winner’:
+    #     messagebox with congratulatory message
+    # else:
+    #     message box with better luck next time message
+    pass
+
 if __name__ == "main":
     pass
